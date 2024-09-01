266febc3635ed9c66b0d7c068040e6a84cea77dd
jatin2511 <jatinsiwach2501@gmail.com>
2024-09-01 14:45:34 +0530

    toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,6 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <ToastContainer/>
+    <ToastContainer style={{zIndex:'100'}}/>
   </StrictMode>,
 )
